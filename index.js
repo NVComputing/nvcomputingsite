7894c49156d477d3935384a9594cf87858f77f12
sting <stinggyray@gmail.com>
2020-07-23 18:21:51 -0500

    force non-www

--- index.js
+++ index.js
@@ -16,9 +16,19 @@ app.use(function forceLiveDomain(req, res, next) {
 	next();
 });
 
+app.use(function wwwRedirect(req, res, next) {
+	if (req.headers.host.slice(0, 4) === 'www.') {
+		let newHost = req.headers.host.slice(4);
+		return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+	}
+	next();
+});
+
 app.use(forceSSL());
 
 app.set('port', process.env.PORT || 8080);
+app.set('trust proxy', true);
+
 app.use(express.static('static'));
 
 routes.set(app);
