f0163f9b473dcae4173741efe2e1c0e1df3a21a8
sting <stinggyray@gmail.com>
2020-07-23 14:10:15 -0500

    force ssl

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 let express = require('express');
 let app = express();
 let hbs = require('hbs');
+let forceSSL = require('heroku-ssl-redirect');
 
 let routes = require('./routes');
+
+/*
+app.use(function forceLiveDomain(req, res, next) {
+	// Don't allow user to hit Heroku
+	let host = req.hostname;
+	if (host === 'nvcomputing.herokuapp.com') {
+		res.redirect(301, 'https://nvcomputing.com' + req.originalUrl);
+	}
+	next();
+});
+ */
+app.use(forceSSL());
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static('static'));
