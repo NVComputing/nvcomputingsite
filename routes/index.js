aa73a64f6b713e851c08f028bbe02f82e24cf3fe
antonbarchukov <52981446+antonbarchukov@users.noreply.github.com>
2020-09-25 11:36:19 -0500

    meeting summaries page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 let basepage = require('./basepage');
 let resources = require('./resources');
 let practice = require('./practice');
-let contact = require('./schedule');
+let schedule = require('./schedule');
+let meetingsummaries = require('./meetingsummaries');
 
 // Load in all endpoints
 module.exports.set = (app) => {
 	basepage.set(app);
 	resources.set(app);
 	practice.set(app);
-	contact.set(app);
+	schedule.set(app);
+	meetingsummaries.set(app);
 
 	console.log("Router endpoints registered.");
 }
