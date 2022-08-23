058e0d84b0518b89ea07aeaf593d61ba8e6dbdce
dylsub <dylansubramanian@gmail.com>
2022-08-22 20:36:07 -0500

    FIXED SCHEDULE TAB WIP

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,35 +1,18 @@
-let tz = require('timezone/loaded');
+let competitions = require('../competitions.json');
 
-let timeStr = '%A, %B %d, %Y - %I:%M %p';
-let schedule = require('../upcomingevents.json');
+competitions.competitions.forEach((competition) => {
+
+
+	// agendaSlide.img = "/res/meetingsummaries/" + agendaSlide.img;
+});
+
 
 module.exports.set = (app) => {
 	app.get('/schedule', (req, res) => {
-		let data = schedule.upcomingEvents.sort((a, b) => {
-			return a.time - b.time;
-		}).map((o) => {
-			let obj = JSON.parse(JSON.stringify(o));
-			if(Date.now() / 1000 >= obj.time) {
-				obj.pastDue = true;
-				obj.notPastDue = false;
-			}
-			obj.time = tz(obj.time * 1000, timeStr, 'en_EN', 'America/Chicago');
-			return obj;
-		});
-
-		let data2 = schedule.deadlines.sort((a, b) => {
-			return a.time - b.time;
-		}).map((o) => {
-			let obj = JSON.parse(JSON.stringify(o));
-			obj.time = tz(obj.time * 1000, timeStr, 'en_EN', 'America/Chicago');
-			return obj;
-		});
-
 		res.render('schedule', {
-			upcomingEvents: data,
-			deadlines: data2,
-			title: "Upcoming Events | Neuqua Valley Computing Team",
+			competitions: competitions.competitions,
+			title: "Schedule | Neuqua Valley Computing Team",
 			schedulePage: true
 		});
-	})
+	});
 }
