c14f8dc336638405db5be1ee0310d2c3602a088c
Kudos <kudosbeluga@gmail.com>
2024-08-11 14:09:55 -0500

    Automatically update leaderboard with class questions data after meeting end

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ let app = express();
 
 let routes = require('./routes');
 var hbs = require('hbs');
-
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const cron = require('node-cron')
 console.log('Setting redirect functions...');
 app.use(function forceDomain(req, res, next) {
 	// Don't allow user to hit Heroku, www-version of site, or http version of site
@@ -34,6 +37,38 @@ app.set('views', './views');
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+
+
+const statsFilePath = path.join(__dirname, 'stats.json')
+
+
+const updateStats = async () => {
+    try {
+        const response = await axios.get('https://autojudgerserver.vercel.app/leaderboards')
+        const leaderboard = response.data
+
+        const stats = JSON.parse(fs.readFileSync(statsFilePath, 'utf-8'))
+
+        stats.users.forEach(user => {
+            if (leaderboard[user.id]) {
+                user.classQuestions = leaderboard[user.id]
+            }
+        })
+
+        fs.writeFileSync(statsFilePath, JSON.stringify(stats, null, 2))
+        console.log('stats.json has been updated successfully.')
+    } catch (error) {
+        console.error('An error occurred while updating stats.json:', error)
+    }
+}
+
+cron.schedule('35 15 * * 4', updateStats, {
+    scheduled: true,
+    timezone: 'America/Chicago'
+})
+
+console.log('Scheduled task to update stats.json with new class questions data every Thursday at 3:35 PM CT.')
 
 let listener = app.listen(app.get('port'), function () {
 	console.log('Express server started.');
